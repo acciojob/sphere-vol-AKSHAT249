ab0dfa6aec16061adef5ef12aaa64dc151713076
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-21 15:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.getElementById("radius");
+	if(radius<0 || isNan(radius)){
+		document.getElementById("volume").textContent = "NaN";
+	}else{
+		let pi = 3.1415927;
+		let volume = Math.round((4*pi*r*r*r)/3,4);
+		document.getElementById("volume").textContent = volume;
+	}
   
 } 
 
